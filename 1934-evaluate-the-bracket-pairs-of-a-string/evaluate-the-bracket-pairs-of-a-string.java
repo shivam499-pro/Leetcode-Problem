edeae08b783ce8bf5a799178663595cda4bd2e9c
Shivam Jaiswal <shivamjaiswal.2024.cse@rajalakshmi.edu.in>
2026-09-26 10:21:09 +0530

    Time: 35 ms (72.40%) | Memory: 91.5 MB (56.56%) - LeetSync

---
+++ 1934-evaluate-the-bracket-pairs-of-a-string/evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,42 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+
+        HashMap<String, String> map = new HashMap<>();
+
+        for (List<String> pair : knowledge) {
+            map.put(pair.get(0), pair.get(1));
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        int i = 0;
+
+        while (i < s.length()) {
+
+            if (s.charAt(i) != '(') {
+                result.append(s.charAt(i));
+                i++;
+            }
+
+            else {
+                int j = i + 1;
+
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+
+                String key = s.substring(i + 1, j);
+
+                if (map.containsKey(key)) {
+                    result.append(map.get(key));
+                } else {
+                    result.append("?");
+                }
+
+                i = j + 1;
+            }
+        }
+
+        return result.toString();
+    }
+}
